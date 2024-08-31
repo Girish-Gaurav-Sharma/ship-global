faedb6e7e5dc61f0ca898c9ca82b3a80fa7551e8
Girish-Gaurav-Sharma <135326401+Girish-Gaurav-Sharma@users.noreply.github.com>
2024-08-31 18:29:40 +0530

    refactor code

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import Category from './components/Category';
-import data from './data.json';
+// import data from './data.json';
 
-
+const data = [
+  "Backlog", "ToDo", "On Going", "Done"
+]
 
 
 function App() {
     return (
         <div className="App" style={{ margin:'0 auto'}}>
          <section className="listlayout" style={{display:'flex', gap:'7px'}}>
-          {data.map((content, ind) => {
+          {data.map((title, ind) => {
           return (
-            <Category key={ind} content={content} />
+            <Category key={ind} title={title} />
           )
          })} 
          </section>
--- todo-list/src/components/Category.jsx
+++ todo-list/src/components/Category.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import TaskItem from './TaskItem';
 import './Category.css'; 
 
-const Category = ({content}) => {
+const Category = ({title}) => {
     const tasks = ['task 1', 'task 2', 'task 3'];
 
     return (
         <div className="category-container">
-            <h3>{key}</h3>
+            <h3>{title}</h3>
             {tasks.map((task, index) => (
                 <TaskItem key={index} task={task} />
             ))}
